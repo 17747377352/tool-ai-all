4dfd80cfbc238f73d9ba98d1e21705ee077bf334
gongxuesong <761164244@qq.com>
2026-02-05 15:50:32 +0800

    切换分支

--- tool-ai-uniapp/unpackage/dist/dev/mp-weixin/common/config/api-config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-const BASE_URL = "http://localhost:18090";
-const apiConfig = {
-  BASE_URL
-};
-exports.apiConfig = apiConfig;
-//# sourceMappingURL=../../../.sourcemap/mp-weixin/common/config/api-config.js.map
